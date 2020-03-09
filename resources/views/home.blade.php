701a22e7183fbb7a908e6ae72045fd6af0a227f4
Lisa Taroli <TaroliFamily@lisas-mbp.myfiosgateway.com>
2020-03-08 22:46:02 -0400

    project skeleton uploaded

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3; pd-4;">
+            <img src="https://www.halloween-website.com/images/glowing-pumpkin.jpg" class="img-rounded">
+        </div>
+        <div class="col-9; pd-3;">
+            <div><h1>Boo Time!</h1></div>
+            <div>
+                <div style="display:inline;" class="pd-5"><strong>153</strong> posts</div>
+                <div style="display:inline;" class="pd-5"><strong>23k</strong> followers</div>
+                <div style="display:inline;" class="pd-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-5 font-weight-bold">BooTime.com</div>
+            <div>We are a community of crazy scary people</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">hi</div>
+        <div class="col-4">hi</div>
+        <div class="col-4">hi</div>
+    </div>
+</div>
+@endsection
